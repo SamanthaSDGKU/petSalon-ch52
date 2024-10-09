59d8f1e405954da74cbdd9dcb7e2b0275635308f
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2024-10-08 19:39:43 -0700

    object literal

---
+++ scripts/practice.js
@@ -0,0 +1,9 @@
+//object literal
+let student1 = {
+    name:"John Smith",
+    age:99,
+    isAdmin:false,
+    grade1:3.7
+}
+
+document.write(student1.isAdmin);
---
+++ scripts/register.js
@@ -0,0 +1,23 @@
+console.log("register.js");
+let pets = [];//empty array
+
+//create pets
+let pet1 = {
+    name:"Scooby",
+    age:99,
+    gender:"Male"
+}
+
+
+
+pets.push(pet1);
+
+function displayNames(){
+    //display pet names
+    console.log(pets[0].name);
+    //console.log(pets[1].name);
+    
+//hint: use a for loop and travel the array
+//hint: use brute force
+//Hint: length the amount of elements on the array
+}
